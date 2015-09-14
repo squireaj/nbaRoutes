806bae0791e8e53d4b32ed5d683a8f391b19cbc5
Squire Johnson <squire.a.johnson@gmail.com>
2015-09-14 01:37:41 -0600

    Finished

--- js/home/homeCtrl.js
+++ js/home/homeCtrl.js
@@ -1,5 +1,19 @@
 var app = angular.module('nbaRoutes');
 
-app.controller('homeCtrl', function($scope, homeService){
+app.controller('homeCtrl', function($scope, teamService, teamData){
+$scope.teamData = teamData;
+console.log(teamData)
+	for(var i = 0; i < $scope.teamData.length; i++){
+		if($scope.teamData[i][1].homeTeam === 'utahjazz'){
+			$scope.teamData[i].homeTeam = 'Utah Jazz',
+			$scope.teamData[i].logoPath = '../images/jazz-logo.png';
+		} else if($scope.teamData[i][1].homeTeam === 'losangeleslakers'){
+			$scope.teamData[i].homeTeam = 'Los Angeles Lakers';
+			$scope.teamData[i].logoPath = '../images/lakers-logo.png';
+		} else if($scope.teamData[i][1].homeTeam === 'miamiheat'){
+			$scope.teamData[i].homeTeam = 'Miami Heat';
+			$scope.teamData[i].logoPath = '../images/heat-logo.png';
+		}
+	}
 
 });
--- js/teams/teamService.js
+++ js/teams/teamService.js
@@ -2,4 +2,68 @@ var app = angular.module('nbaRoutes');
 
 app.service('teamService', function($http, $q){
 
-});
+	this.addNewGame = function(gameObj) {
+		var url = "https://api.parse.com/1/classes/" + gameObj.homeTeam;
+		if (parseInt(gameObj.homeTeamScore) > parseInt(gameObj.opponentScore)) {
+				gameObj.won = true;
+		}
+		else {
+				gameObj.wone = false;
+		}
+		return $http({
+			method: 'POST',
+			url: url,
+			data: gameObj
+		})
+
+	} //***** addNewGame
+
+	this.getTeamData = function(team) {
+		var deferred = $q.defer();
+		var url = 'https://api.parse.com/1/classes/' + team;
+		$http({
+			method: 'GET',
+			url: url,
+		}).then(function(data) {
+			var results = data.data.results;
+			var wins = 0;
+			var losses = 0;
+			for(var i = 0; i < results.length; i++) {
+				if(results[i].won === true){
+					wins++;
+				}
+				else {
+					losses++;
+				}	
+			}
+			results.wins = wins;
+			results.losses = losses;
+			deferred.resolve(results);
+		})
+		return deferred.promise;
+	} //***** getTeamData
+
+		this.getAllData = function(){
+		var promises = [this.getTeamData('utahjazz'), this.getTeamData('losangeleslakers'), this.getTeamData('miamiheat')];
+		console.log(promises);
+		return $q.all(promises);
+	}
+
+
+}); //End
+
+
+  // this.getUsers = function() {
+  // 	var dfd = $q.defer();
+  //   $http({
+  //       method: 'GET',
+  //       url: 'http://reqr.es/api/users?page=1'
+  //   }).then(function(responce) {
+  //   	parsedResponse = responce.data.data;
+  //   	for (var i = 0; i < parsedResponse.length; i++) {
+  //   		parsedResponse[i].first_name = 'Ralf';
+  //   	}
+  //   	dfd.resolve(parsedResponse)
+  //   })
+  //   return dfd.promise;
+  // }
